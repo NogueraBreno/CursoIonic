d4a9fc0612c1c8b886d4a6fc8ce2c0a3dda25beb
Breno Da Silva Nogueira <05589608120@sinfra.mt.gov.br>
2018-07-18 14:07:53 -0400

    update 30

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,10 +13,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [MovieProvider]
+  
 })
 export class FeedPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public obj_feed = {
+
+    title: "Gil Brother",
+    date: "November 5, 1955",  
+    desc: "Não aguenta 10 minutos de porrada comigo!",
+    qtd_likes: 12,
+    qtd_cmts:4,
+    cmts_time: 11
+  }
+
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams,
+     private movieProvider : MovieProvider) {
   }
   username = "Gil Brother";
   public somaDeDoisNumeros(num1:number,num2:number):  void{
@@ -25,6 +40,8 @@ export class FeedPage {
 
   ionViewDidLoad() {
 //this.somaDeDoisNumeros(5,5);
+this.movieProvider.getLatestMovies();
+
     
 }
 
